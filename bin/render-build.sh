49e155a16c956c2ccb7b37ba0f1714a580197bfe
ynhiine <yeenki1107@gmail.com>
2025-01-06 16:24:16 +0700

    add user ad

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,6 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
-bundle exec rake db:migrate db:seed db:create_admin_user
+bundle exec rails db:migrate db:seed db:create_admin_user
 
 
