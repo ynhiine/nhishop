96a4a15793788b73c351bf66ec6d119649d914bf
ynhiine <yeenki1107@gmail.com>
2025-01-06 16:49:50 +0700

    Fix render-build.sh and Procfile

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,10 +1,12 @@
+#!/bin/sh
+set -e
 
-set -o errexit
+# Install gems
 bundle install
+
+# Precompile assets
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+
+# Migrate the database
 bundle exec rails db:migrate
-web: bundle exec rails server
-release: bundle exec rails runner 'require_relative "./script/create_user"'
-
-
